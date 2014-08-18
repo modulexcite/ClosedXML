8f7002b6b4bdf927078985a6de390dfd9c501f21
Jay Asbury <vbjay.net@gmail.com>
2014-08-18 14:08:15 -0400

    Add validation to workbook.

---
+++ ClosedXML/ClosedXML/ClosedXML/Excel/XLWorkbook_Validation.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Validation;
+
+namespace ClosedXML.Excel
+{
+    public partial class XLWorkbook
+    {
+        private static IEnumerable<ValidationErrorInfo> GetValidationErrors(SpreadsheetDocument package)
+        {
+            OpenXmlValidator validator = new OpenXmlValidator();
+            if (package == null) return Enumerable.Empty<ValidationErrorInfo>();
+            return validator.Validate(package);
+        }
+
+        private static bool isValid(SpreadsheetDocument package)
+        {
+            return GetValidationErrors(package).Count() == 0;
+        }
+
+        public static bool isValid(String filePath)
+        {
+            using (var package = SpreadsheetDocument.Open(filePath, false))
+            {
+                return isValid(package);
+            }
+        }
+
+        public static IEnumerable<ValidationErrorInfo> GetValidationErrors(String filePath)
+        {
+            using (var package = SpreadsheetDocument.Open(filePath, false))
+            {
+                return GetValidationErrors(package);
+            }
+        }
+    }
+}
